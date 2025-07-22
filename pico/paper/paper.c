b87b753dc779a97d2b4b9d1fc11110bae8711412
Ben Foxall <benfoxall@gmail.com>
2025-07-22 10:54:04 +0100

    Link up example

--- pico/paper/paper.c
+++ pico/paper/paper.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-
-
+#include "EPD_Test.h" //Examples
 
 int main()
 {
     stdio_init_all();
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+    EPD_2in13_V2_test();
+
+    // while (true) {
+    //     printf("Hello, world!\n");
+    //     sleep_ms(1000);
+    // }
 }
